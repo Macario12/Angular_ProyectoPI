405bc2c7245a5a87d86aa1b509e32f02ec45674d
Brian Erazo Sagastume <3012750170101@ingenieria.usac.edu.gt>
2020-11-10 00:13:29 -0600

    vista de editar perfil

--- ProyectoangularPI/src/app/editar-perfil/editar-perfil.component.ts
+++ ProyectoangularPI/src/app/editar-perfil/editar-perfil.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute, Params, RouterLink} from "@angular/router";
+import { usuario } from '../modelos/usuario.model';
+import { UsuarioService } from '../services/usuario.service';
+
 
 @Component({
   selector: 'app-editar-perfil',
   templateUrl: './editar-perfil.component.html',
-  styleUrls: ['./editar-perfil.component.css']
+  styleUrls: ['./editar-perfil.component.css'],
+  providers: [UsuarioService]
 })
 export class EditarPerfilComponent implements OnInit {
 
